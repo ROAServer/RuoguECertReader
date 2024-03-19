a170054117501cafe0600e04abc10733c8160733
ZhuRuoLing <jkl-963@live.com>
2024-03-19 12:40:28 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "icu.takeneko.eCert.reader"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 15
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
